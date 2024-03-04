127fec78bba5035461872b78639be14495001353
arnauggX <lpac7697@gmail.com>
2024-03-04 12:48:07 +0100

    Initial code final

---
+++ Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int opcion;
+        do {
+            System.out.println("1. Consultar saldo");
+            System.out.println("2. Blackjack");
+            System.out.println("3. Slot");
+            System.out.println("4. Ruleta");
+            System.out.println("5. Salir");
+            System.out.print("Elige una opción: ");
+            opcion = scanner.nextInt();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    blackjack();
+                    break;
+                case 3:
+                    slot();
+                    break;
+                case 4:
+                    ruleta();
+                    break;
+                case 5:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, elige una opción del 1 al 5.");
+            }
+        } while(opcion != 5);
+
+    }
+
+    public static void blackjack() {
+    }
+
+    public static void slot() {
+    }
+
+    public static void ruleta() {
+    }
+}
