8b86578a5580a612ca94a36591d0f3e1a78ef185
arnauggX <lpac7697@gmail.com>
2024-03-08 10:21:06 +0100

    Code V3 (Metodo solicitarApuesta y obtenerApuesta hechos)

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 
 public class Main {
-    private static double saldo = 100.0;
+    private static double saldo = 500.0;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -36,7 +36,7 @@ public class Main {
                 default:
                     System.out.println("Opción no válida. Por favor, elige una opción del 1 al 5.");
             }
-        } while(opcion != 5);
+        } while (opcion != 5);
 
     }
 
@@ -73,4 +73,48 @@ public class Main {
 
     public static void ruleta() {
     }
+
+
+    private static double solicitarApuesta(Scanner scanner) {
+        System.out.println("Seleccione la cantidad de apuesta:");
+        System.out.println("1. $0.2");
+        System.out.println("2. $0.4");
+        System.out.println("3. $0.8");
+        System.out.println("4. $1");
+        System.out.println("5. $2");
+        System.out.println("6. $5");
+        System.out.println("7. $10");
+        System.out.println("8. $50");
+        System.out.println("9. $100");
+
+        int opcionApuesta = scanner.nextInt();
+        return obtenerApuesta(opcionApuesta);
+    }
+
+    private static double obtenerApuesta(int opcion) {
+        switch (opcion) {
+            case 1:
+                return 0.2;
+            case 2:
+                return 0.4;
+            case 3:
+                return 0.8;
+            case 4:
+                return 1.0;
+            case 5:
+                return 2.0;
+            case 6:
+                return 5.0;
+            case 7:
+                return 10.0;
+            case 8:
+                return 50.0;
+            case 9:
+                return 100.0;
+            default:
+                System.out.println("Opción no válida. Apuesta establecida a $0.0");
+                return 0.0;
+        }
+    }
+
 }
